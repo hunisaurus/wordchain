7f27fb26013c434488b0ceb04fb7d27f6460afc5
Zoltan Imre <pugomagitor@gmail.com>
2024-11-26 23:47:00 +0100

    Added player 2

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync({sigint: true});
 import dictionary from "an-array-of-english-words" with { type: "json" };
+let player2;
 
 //szó array
 let words = [];
 let word1;
 //első szó:
+
+let howmany = prompt("Do you wish to have a second player join?")
+while (howmany.toLowerCase() !== "yes" && howmany.toLowerCase() !== "no") {
+    howmany = prompt("Do you wish to have a second player join? Yes or No?")
+}
+
+if (howmany.toLowerCase() === "yes") {
+    player2 = true;
+}
+
 console.clear();
 console.log("Welcome to the Word Chain game!");
 while (!(dictionary.includes(word1))) {
@@ -33,7 +44,7 @@ console.log(words);
 for (let i = 0; i < words.length; i++){
     word1 = "";
     let lastLetter = words[i][words[i].length - 1];
-    while (word1 === "" || word1[0] !== lastLetter || !(isNaN(word1)) || !(dictionary.includes(word1)) || words.includes(word1)){
+    while (word1 === "" || word1[0] !== lastLetter || !(isNaN(word1)) || !(dictionary.includes(word1)) || words.includes(word1)|| word1 === "!show_1"|| word1 === "!show_2"){
         console.log("\nYour previous word is: " + words[i]);
         console.log(`(To quit, type "!quit")`);
         console.log(`\nGive me the next word, starting with the letter "` + lastLetter + `"`);
@@ -42,6 +53,18 @@ for (let i = 0; i < words.length; i++){
         if (word1 === "!quit"){
             console.log("\nThank you for playing! BYE!");
             process.exit();
+        } else if (word1 === "!show_1") {
+            console.log("Player 1 has given the following words:")
+            for (let index = 0; index < words.length; index+=2) {
+                const element = words[index];
+                console.log(words[index])
+            }
+        } else if (word1 === "!show_2" && player2 === true) {
+            console.log("Player 2 has given the following words:")
+            for (let index = 1; index < words.length; index+=2) {
+                const element = words[index];
+                console.log(words[index])
+            }
         } else if (word1 === undefined || isNaN(word1) !== true) {
             console.log("\nPlease, write a valid word!");
         } else if (!(dictionary.includes(word1))){
